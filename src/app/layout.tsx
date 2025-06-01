364591f5553326893fc5d69aed9f51b7a1c5a48d
tolik <tol.kowalev@gmail.com>
2025-06-01 09:43:24 +0300

    feat: Improve accessibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,10 +33,7 @@ export default function RootLayout({
   return (
     <html lang="uk" className={`${rubik.variable} ${lora.variable}`}>
       <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="font-rubik text-foreground antialiased light">
         <AppLayout>{children}</AppLayout>
